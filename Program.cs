2ff268b73be10bf4dae343806fd1fa5dd36aa579
JoaoPedroCordoba <jp.8pedro86@gmail.com>
2022-03-25 21:44:20 -0300

    finalizadoexercicio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Double j, c, i, m, t;
+
+Console.WriteLine("Capital [c] (R$).......:");
+c = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Taxa de juros [i] (%)..:");
+i = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Tempo [t] (meses)......:");
+t = Convert.ToDouble(Console.ReadLine());
+
+j = c* (i / 100)*t;
+m = (c + j);
+
+Console.WriteLine($"Juros (R$).....:{j:C2}");
+Console.WriteLine($"Montante (R$)..:{m:C2}");
